08d7b1cf87a239ea47277711ab38be2153260529
zainmusafir <zainfarooqi.master@gmail.com>
2025-07-22 14:49:25 +0200

    new readme

--- min-katt/src/extension.ts
+++ min-katt/src/extension.ts
@@ -1,33 +1,4 @@
 
-/*
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "min-katt" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('min-katt.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from min_katt!');
-	});
-
-	context.subscriptions.push(disposable);
-}
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
-
-*/
 
 import * as vscode from 'vscode';
 import * as path from 'path';
@@ -35,39 +6,46 @@ import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log("🚀 Extension activated");
-    const disposable = vscode.commands.registerCommand('min-katt.showCat', () => {
+
+	const disposable = vscode.commands.registerCommand('min-katt.showCat', () => {
 		console.log("🎯 Command min-katt.showCat triggered");
-        const panel = vscode.window.createWebviewPanel(
-            'minKattCatPanel',
-            'Meow! 🐱',
-            vscode.ViewColumn.One,
-            {
-                enableScripts: true,
-                localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'media'))]
-            }
-        );
 
-        const mediaPath = path.join(context.extensionPath, 'media');
-        const files = fs.readdirSync(mediaPath).filter(file => /\.(gif|png|jpe?g)$/i.test(file));
-        const randomFile = files[Math.floor(Math.random() * files.length)];
-        //const catUri = vscode.Uri.file(path.join(mediaPath, randomFile)).with({ scheme: 'vscode-resource' });
-		const catPath = vscode.Uri.file(path.join(mediaPath, randomFile));
-		const catUri = panel.webview.asWebviewUri(catPath);
+		const mediaPath = path.join(context.extensionPath, 'media');
 
-        panel.webview.html = getCatHtml(catUri.toString(), randomFile);
+		let files: string[];
+		try {
+			files = fs.readdirSync(mediaPath).filter(file => /\.(gif|png|jpe?g)$/i.test(file));
+		} catch (err) {
+			vscode.window.showErrorMessage(`Error reading media folder: ${err}`);
+			return;
+		}
 
-		console.log("Extension activated");
-
-		console.log("All image files:", files); // <-- Add this
 		if (files.length === 0) {
-   		 vscode.window.showErrorMessage("No cat images found in media/");
-    	return;
-}
+			vscode.window.showErrorMessage("No cat images found in media/");
+			return;
+		}
 
+		const randomFile = files[Math.floor(Math.random() * files.length)];
+		const filePath = path.join(mediaPath, randomFile);
+		const fileUri = vscode.Uri.file(filePath);
 
-    });
+		const panel = vscode.window.createWebviewPanel(
+			'minKattCatPanel',
+			'Meow! 🐱',
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true,
+				localResourceRoots: [vscode.Uri.file(mediaPath)]
+			}
+		);
 
-    context.subscriptions.push(disposable);
+		const catWebviewUri = panel.webview.asWebviewUri(fileUri);
+		console.log("🐱 Selected cat:", catWebviewUri.toString());
+
+		panel.webview.html = getCatHtml(catWebviewUri.toString(), randomFile);
+	});
+
+	context.subscriptions.push(disposable);
 }
 
 function getCatHtml(catUri: string, name: string) {
